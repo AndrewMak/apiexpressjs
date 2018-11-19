a21bd90bbf4491a1731f2bebf00918dbe2c0efc6
Andrew <andrew.cm.sp@gmail.com>
2018-11-19 20:39:57 -0200

    enable cors

--- index.js
+++ index.js
@@ -7,6 +7,11 @@ const app = express();
 //bodyparser Middleware
 app.use(bodyParser.json());
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 // db config
 const db = require('./keys').mongoURI;
 
